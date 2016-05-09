b9d5e69783984756abec5f3639d641cc2f8dbe6d
bttf <adnanchowdhury88@gmail.com>
2016-05-09 10:15:07 -0700

    check for undefined in helper

--- addon/helpers/normalize-url.js
+++ addon/helpers/normalize-url.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import _normalizeUrl from 'normalize-url';
 
 export function normalizeUrl(params, options) {
-  return _normalizeUrl(...params, options);
+  return params.length && params[0] ? _normalizeUrl(...params, options) : '';
 }
 
 export default Ember.Helper.helper(normalizeUrl);
