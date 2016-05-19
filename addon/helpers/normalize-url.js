c156f220d14e80e0b3f349d3be099d466e34a7b6
bttf <adnanchowdhury88@gmail.com>
2016-05-19 16:38:14 -0700

    quick patch

--- addon/helpers/normalize-url.js
+++ addon/helpers/normalize-url.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 import _normalizeUrl from 'normalize-url';
 
 export function normalizeUrl(params, options) {
-  return params.length && params[0] ? _normalizeUrl(...params, options) : '';
+  try {
+    return params.length && params[0] ? _normalizeUrl(...params, options) : '';
+  } catch (error) {
+    return '';
+  }
+  //return params.length && params[0] ? _normalizeUrl(...params, options) : '';
 }
 
 export default Ember.Helper.helper(normalizeUrl);
